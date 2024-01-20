0b3aa653fbb66e91685617d9d80b3fa7eba07ce9
jmvsta <65613626+jmvsta@users.noreply.github.com>
2024-01-20 22:32:46 +0100

    pre prefabs added, score calculation, cubics y corrected

--- Assets/Scripts/RoadSpawner.cs
+++ Assets/Scripts/RoadSpawner.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -6,6 +5,8 @@ using UnityEngine.UI;
 public class RoadSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] _roadPrefabs;
+    [SerializeField] private GameObject _roadPrefab1;
+    [SerializeField] private GameObject _roadPrefab2;
     [SerializeField] private GameObject _spawnObj;
     [SerializeField] private Text _scoreText;
     [SerializeField] private float _spawnPos = 0;
@@ -13,7 +14,7 @@ public class RoadSpawner : MonoBehaviour
     private List<GameObject> _activeRoad = new List<GameObject>();
     private float _roadLenght = 100;
     private int _startRoad = 5;
-    private int _score;
+    private float _score;
 
     public float Speed {get { return _speed; } private set { Speed = _speed; } }
 
@@ -22,7 +23,14 @@ public class RoadSpawner : MonoBehaviour
     {
         for (int i = 0; i < _startRoad; i++)
         {
-            SpawnRoad();
+            if (i == 0 || i == 1)
+            {
+                SpawnRoad(i);
+            }
+            else
+            {
+                SpawnRoad();
+            }
             _spawnPos += _roadLenght;
         }
 
@@ -32,6 +40,12 @@ public class RoadSpawner : MonoBehaviour
     public void SpawnRoad()
     {
         GameObject _nextRoad = Instantiate(_roadPrefabs[Random.Range(0, _roadPrefabs.Length)], transform.forward * _spawnPos, Quaternion.identity);
+        _activeRoad.Add(_nextRoad);
+    }
+
+    public void SpawnRoad(int roadId)
+    {
+        GameObject _nextRoad = Instantiate(roadId == 1 ? _roadPrefab1 : _roadPrefab2, transform.forward * _spawnPos, Quaternion.identity);
         _activeRoad.Add(_nextRoad);
     }
 
@@ -45,8 +59,8 @@ public class RoadSpawner : MonoBehaviour
     {
         if (Time.timeScale != 0)
         {
-            _score += _speed;
-            _scoreText.text = _score.ToString();
+            _score += (float)_speed / 10;
+            _scoreText.text = (System.Math.Round(_score, 0)).ToString();
         }
     }
 }
