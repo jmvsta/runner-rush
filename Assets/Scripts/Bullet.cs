43cc85bdc75fe789571d273cb90a794f502a483b
Anastasia Zharikova <ADZharikova@yandex.ru>
2024-07-14 15:28:12 +0300

    cool shooting

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,19 +1,35 @@
+using Spawn;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    [SerializeField] private float life = 3;
+    [SerializeField] private EnemiesSpawner _enemiesSpawner;
+    [SerializeField] private ObstaclesSpawner _obstaclesSpawner;
+    [SerializeField] private float life = 10;
 
     void Awake()
     {
         Destroy(gameObject, life);
     }
 
-    void OnCollisionEnter(Collision collision)
+
+    void Start()
     {
-        Destroy(collision.gameObject);
-        Destroy(gameObject);
+        _enemiesSpawner = GameObject.Find("EnemiesSpawner").GetComponent<EnemiesSpawner>();
+        _obstaclesSpawner = GameObject.Find("ObstaclesSpawner").GetComponent <ObstaclesSpawner>();
+    }
+
+        private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Died":
+                //_obstaclesSpawner.KillEnemy(other);
+                _enemiesSpawner.KillEnemy(other);
+                Destroy(gameObject);
+                break;
+        }
     }
 }
