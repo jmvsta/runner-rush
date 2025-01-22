470a304a6d00be178ae32f4f8411d35d930a0bd0
jmvsta <65613626+jmvsta@users.noreply.github.com>
2025-01-22 02:39:01 +0100

    added animation and random movement of crab

---
+++ Assets/Scripts/Destructible/Behaviour/MoveObjectX.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+namespace Destructible.Behaviour
+{
+    public class MoveObjectX : MonoBehaviour
+    {
+        
+        [SerializeField] private float _speed = 10.0f;
+        private readonly Random _random = new();
+
+        private void FixedUpdate()
+        {
+            if (transform.position.x < -5)
+            {
+                transform.position += Vector3.right * _random.Next(0, 6) * Time.fixedDeltaTime;
+            }
+            else if (transform.position.x > 5)
+            {
+                transform.position += Vector3.right * _random.Next(-5, 1) * Time.fixedDeltaTime;
+            }
+            else
+            {
+                transform.position += Vector3.right * _random.Next(-2, 3) * _speed * Time.fixedDeltaTime;
+            }
+            
+        }
+    }
+}
